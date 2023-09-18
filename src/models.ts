c81b04b37c9abc8fb0279d0d3d3a3cf2e87e4286
Sergey Bespalko <bespalkoser@gmail.com>
2023-09-19 02:02:44 +0300

    clean up

--- src/models.ts
+++ src/models.ts
@@ -1,7 +1,33 @@
 import { MonoEventTypeEnum } from '@prisma/client'
+export type RawMonoEvent =
+  | {
+      data?: RawMonoEventData
+      type?: MonoEventTypeEnum & string
+      fromIp?: string
+    }
+  | undefined
 
-export type RawMonoEvent = {
-  data: object
-  type: MonoEventTypeEnum & string
-  fromIp?: string
-}
+export type RawMonoEventData =
+  | {
+      account?: string
+      statementItem?: RawMonoEventStatementItem
+    }
+  | undefined
+
+export type RawMonoEventStatementItem =
+  | {
+      id?: string
+      mcc?: number
+      hold?: boolean
+      time?: number
+      amount?: number
+      balance?: number
+      receiptId?: string
+      description?: string
+      originalMcc?: number
+      currencyCode?: number
+      cashbackAmount?: number
+      commissionRate?: number
+      operationAmount?: number
+    }
+  | undefined
--- src/mono-events.repository.ts
+++ src/mono-events.repository.ts
@@ -1,31 +1,62 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { DatabaseService } from './database.service'
 import { RawMonoEvent } from './models'
-import { MonoEventTypeEnum, MonoRootEvent } from '@prisma/client'
+import { MonoEventTypeEnum } from '@prisma/client'
 
 @Injectable()
 export class MonoEventsRepository {
-  private readonly lg = new Logger(MonoEventsRepository.name)
   private readonly unknownEvents = this.__db.monoUnknownEvent
-  private readonly rootEvents = this.__db.monoRootEvent
+  private readonly statementItems = this.__db.monoEventStatementItem
   constructor(private readonly __db: DatabaseService) {}
 
   public async create(event: RawMonoEvent) {
     try {
       if (this.isMonoRootEventStatementItem(event)) {
-        this.lg.log(JSON.stringify(event))
-        return await this.rootEvents.create({ data: event })
+        const { account = '', statementItem } = event?.data ?? {}
+        const {
+          id = '',
+          mcc = 0,
+          hold = false,
+          time = 0,
+          amount = 0,
+          balance = 0,
+          receiptId = '',
+          description = '',
+          originalMcc = 0,
+          currencyCode = 0,
+          cashbackAmount = 0,
+          commissionRate = 0,
+          operationAmount = 0
+        } = statementItem ?? {}
+
+        return await this.statementItems.create({
+          data: {
+            type: MonoEventTypeEnum.StatementItem,
+            account,
+            monoId: id,
+            mcc,
+            hold,
+            time,
+            amount,
+            balance,
+            receiptId,
+            description,
+            originalMcc,
+            currencyCode,
+            cashbackAmount,
+            commissionRate,
+            operationAmount
+          }
+        })
       }
 
-      this.lg.log('isNOT')
-      return await this.unknownEvents.create({ data: { event } })
-    } catch (e) {
-      console.log(e)
+      return await this.unknownEvents.create({ data: { event: JSON.stringify(event) } })
+    } catch {
       return null
     }
   }
 
-  private isMonoRootEventStatementItem(c: unknown): c is MonoRootEvent {
-    return (c as MonoRootEvent)?.type === MonoEventTypeEnum.StatementItem
+  private isMonoRootEventStatementItem(c: unknown): c is RawMonoEvent {
+    return (c as RawMonoEvent)?.type === MonoEventTypeEnum.StatementItem
   }
 }
