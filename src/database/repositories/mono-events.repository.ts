371a0ce3175cfd8e84a4e7a184c44a2ed47df773
Sergey Bespalko <bespalkoser@gmail.com>
2023-09-21 19:32:01 +0300

    clean up

--- src/database/repositories/mono-events.repository.ts
+++ src/database/repositories/mono-events.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { DatabaseService } from '../database.service'
 import { RawMonoEvent } from '../../models/models'
-import { MonoEventTypeEnum, MonoEventStatementItem, MonoUnknownEvent } from '@prisma/client'
+import { MonoEventTypeEnum, MonoEventStatementItem, MonoUnknownEvent, Prisma } from '@prisma/client'
 import { TelegramService } from '../../telegram/telegram.service'
 import { getGrn, getIsPosMoneyOperation, getMoneyOperationEmoji } from '../../../utils/utils'
 import { STICKERS_MAP, UNKNOWN_EMOJI } from '../../../constants/constants'
@@ -35,7 +35,7 @@ export class MonoEventsRepository {
           operationAmount = 0
         } = statementItem ?? {}
 
-        this.telegram.log(
+        await this.telegram.log(
           'MonoEventsRepository',
           [
             `Description: <tg-spoiler>${description}</tg-spoiler>`,
@@ -66,12 +66,20 @@ export class MonoEventsRepository {
         })
       }
 
-      this.telegram.log('MonoEventsRepository', `Unknown event: ${event?.['type'] ?? UNKNOWN_EMOJI}`)
+      await this.telegram.log('MonoEventsRepository', `Unknown event: ${event?.['type']} ${UNKNOWN_EMOJI}`)
       this.telegram.sendSticker(STICKERS_MAP.UNKNOWN_OPERATION)
       return this.unknownEvents.create({ data: { event: JSON.stringify(event) } })
     } catch (e) {
-      this.telegram.log('MonoEventsRepository', `<pre>${JSON.stringify(e, null, 2)}</pre>`)
-      this.telegram.sendSticker(STICKERS_MAP.NOT_POSITIVE_OPERATION_AMOUNT)
+      let text = 'Unknown prisma error'
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError ||
+        e instanceof Prisma.PrismaClientValidationError ||
+        e instanceof Prisma.PrismaClientUnknownRequestError
+      ) {
+        text = e.message
+      }
+      await this.telegram.log('MonoEventsRepository', text)
+      this.telegram.sendSticker(STICKERS_MAP.ERROR_IN_OPERATION)
       return null
     }
   }
